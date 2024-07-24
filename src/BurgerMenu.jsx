cc35a8566270f21a344fb786a137e6da558b0519
sagarkarki903 <sagarkarki903@gmail.com>
2024-07-24 01:35:11 -0500

    test6

--- src/BurgerMenu.jsx
+++ src/BurgerMenu.jsx
@@ -29,7 +29,7 @@ export default function BurgerMenu({setBBurgerOpen}){
                 <HashLink to={`/#projects`} onClick={handleLinkClick}><li className="text-center">Projects</li></HashLink>
                 <HashLink to={`/#contact`} onClick={handleLinkClick}><li className="text-center">Contact</li></HashLink>
                 <Link to={`/log`} onClick={handleLinkClick}><li><button className="bg-blue-500 hover:bg-blue-700 text-white text-center font-bold py-2 px-4 rounded-full">Sign In</button></li></Link>
-                
+                <Link to={`admin`}>ADMIN</Link>
 
          
          </>
